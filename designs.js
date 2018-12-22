2fb4814e97f2d32c6537bc3eb227562b1c972148
TaoRan <869613017@qq.com>
2018-12-22 10:30:18 +0800

    js:add $(function) and function makeGrid();html:add value="#000000"

--- designs.js
+++ designs.js
@@ -3,8 +3,23 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+$(function(){
+    // 点击提交按钮，生成对应画布单元格
+    $('#submit').click(function makeGrid(){
+        // 定义变量，存放两个输入框的位置，提交按钮的位置
+        let height, width, table;
+        height = $('#inputHeight');
+        width = $('#inputWidth');
+        table = $('#pixelCanvas');
+        // 每次生成新画布前先删除旧画布
+        table.find('tr', 'td').remove();
+        // 获取输入框的行数，循环生成对应数目的行数
+        for(let m = 0; m < height.val(); m++){
+            table.append('<tr></tr>');
+        }
+        // 获取输入框的列数，循环生成对应数目的列数
+        for(let n = 0; n < width.val(); n++){
+            $('tr').append('<td></td>')
+        }
+    })
+});
